bac31ba7d1620a70d83c89a0b30c68e933d9244b
Galc3882 <86870298+Galc3882@users.noreply.github.com>
2022-05-23 13:42:00 -0400

    Added Feature Similarity, search  similar songs algorithm and changed database to example for testing
    Feature Similarity houses the similarity functions

---
+++ SAR/FeatureSimilarity.py
@@ -0,0 +1,16 @@
+# All returns are between 0 and 1.
+
+def key(key1, key2):
+    '''
+    This function takes two keys and returns the similarity between them.
+    '''
+    return max(1-0.1*abs(int(key1) - int(key2)), 0)
+
+def tempo(tempo1, tempo2):
+    '''
+    This function takes two tempos and returns the similarity between them.
+    '''
+    return max(1-0.01*abs(float(tempo1) - float(tempo2)), 0)
+
+# A dictionary of the similarity functions
+methodDictionary = {1: key, 2:tempo}
--- SAR/Search.py
+++ SAR/Search.py
@@ -1,7 +1,11 @@
+from cmath import sqrt
 import csv
 import os
+from xml.etree.ElementInclude import include
 from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+import numpy as np
+import FeatureSimilarity
+
 
 
 def fuzzySearchSongTitle(songTitle, data, threshold=60):
@@ -13,7 +17,7 @@ def fuzzySearchSongTitle(songTitle, data, threshold=60):
     maxRatio = [0, 0]
 
     for row in range(len(data)):
-        ratio = fuzz.ratio(songTitle, data[row][50])  # 50 is the song title
+        ratio = fuzz.ratio(songTitle, data[row][0])  # 50 is the song title
         if ratio > threshold and ratio > maxRatio[0]:
             maxRatio[0] = ratio
             maxRatio[1] = row
@@ -22,6 +26,43 @@ def fuzzySearchSongTitle(songTitle, data, threshold=60):
     print("Ratio of Fuzzy Search: " + str(maxRatio[0])) # print the ratio of the song
     return data[maxRatio[1]]
 
+def findSimilarSongs(song, numOfSongs, data):
+    """
+    Finds the most similar songs to the song at the index.
+    Returns the most similar songs using cosinw similarity.
+    """
+    # Cap the number of songs to be returned
+    if numOfSongs > len(data):
+        numOfSongs = len(data)-1
+    # Calculate the cosine similarity between the song and all the songs in the database
+    Songs = [[], []]
+    for row in range(len(data)):
+        if data[row][0] != song[0]:
+            Songs[0].append(data[row])
+            Songs[1].append(cosineSimilarity(song, data[row]))
+    
+    # pick numOfSongs most similar songs
+    similarSongs = []
+    for i in range(numOfSongs):
+        maxIndex = Songs[1].index(max(Songs[1]))
+        similarSongs.append([Songs[0][maxIndex], Songs[1][maxIndex]])
+        Songs[1][maxIndex] = 0
+    return similarSongs
+
+def cosineSimilarity(song1, song2):
+    """
+    Calculates the cosine similarity between two songs.
+    Returns the similarity value.
+    """
+    # Vector of weights for each feature
+    weights = [1.3, 1] # [1 for i in range(len(song1)-1)]
+
+    # Calculate the dot product of the two songs
+    similarities = []
+    for i in range(1, len(song1)):
+        similarities.append(FeatureSimilarity.methodDictionary[i](song1[i], song2[i]))
+    # Return dot product of weights and similarities
+    return np.dot(weights, similarities)/sum(weights)
 
 if __name__ == '__main__':
     # Read in the csv file
@@ -39,5 +80,11 @@ if __name__ == '__main__':
     if (songAnalysis == None):
         print("Song not found within the threshold")
     else:
-        print("Found Song: " + songAnalysis[50]) # b'Amor De Cabaret'
+        print("Found Song: " + songAnalysis[0]) # b'Amor De Cabaret'
 
+    # Find most similar song using cosine similarity
+    similarSongs = findSimilarSongs(songAnalysis, 5, data)
+    print("Similar Songs: ")
+    for i in range(len(similarSongs)):
+        print(similarSongs[i][0][0])
+        print("Cosine Similarity: " + str(similarSongs[i][1]))
