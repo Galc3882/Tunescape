9eb22c2eb879d728510417672b772a92acd464eb
Galc3882 <86870298+Galc3882@users.noreply.github.com>
2022-06-04 15:25:35 -0400

    Changed formatting in builder, added database analysis

---
+++ SAR/databaseAnalysis.py
@@ -0,0 +1,53 @@
+import pickle
+import csv
+
+if __name__=='__main__':
+    with open('database.pickle', 'rb') as handle:
+        database = pickle.load(handle)
+
+    cropped_getters = ['title', 'artist_name', 'duration', 'key', 'key_confidence', 'mode',
+                       'mode_confidence', 'tempo', 'loudness', 'time_signature', 
+                       'time_signature_confidence', 'year', 'sections_start', 'segments_pitches', 
+                       'segments_timbre', 'bars_start', 'bars_confidence', 'tatums_confidence', 
+                       'tatums_start']
+
+    i = 0
+    # print dictionary in nice format
+    for x in database.items():
+        if i<1:
+            # print('Song: ' + x[1][0])
+            # print('Band: ' + x[1][1])
+            # print('Duration: ' + str(x[1][2]))
+            # print('Key and confidence: ' + str(x[1][3]) + '\t' + str(x[1][4]))
+            # print('Mode and confidence: ' + str(x[1][5]) + '\t' + str(x[1][6]))
+            # print('Tempo: ' + str(x[1][7]))
+            # print('Loudness: ' + str(x[1][8]))
+            # print('Time signature and confidence: ' + str(x[1][9]) + '\t' + str(x[1][10]))
+            # print('Year: ' + str(x[1][11]))
+            # print('Sections start: ')
+            # print(*x[1][12])
+
+            # print('Segments pitches: ')
+            # for y in x[1][13]:
+            #     print(*y)
+
+            # print('Segments timbre: ')
+            # for y in x[1][14]:
+            #     print(*y)
+
+            # print('Bars start: ')
+            # print(*x[1][15])
+
+            # print('Bars confidence: ')
+            # print(*x[1][16])
+
+            # print('Tatums confidence: ')
+            # print(*x[1][17])
+
+            print('Tatums start: ')
+            print(*x[1][18])
+            # print('\n')
+        else:
+            break
+        i+=1
+
--- The 1 Million Song Database/Builder.py
+++ The 1 Million Song Database/Builder.py
@@ -30,8 +30,33 @@ def getFeatures(hdf5_file):
 
     h5.close()
 
+    """ 
+    ['get_analysis_sample_rate', 'get_artist_7digitalid',
+       'get_artist_familiarity', 'get_artist_hotttnesss', 'get_artist_id',
+       'get_artist_latitude', 'get_artist_location',
+       'get_artist_longitude', 'get_artist_mbid', 'get_artist_mbtags',
+       'get_artist_mbtags_count', 'get_artist_name',
+       'get_artist_playmeid', 'get_artist_terms', 'get_artist_terms_freq',
+       'get_artist_terms_weight', 'get_audio_md5', 'get_bars_confidence',
+       'get_bars_start', 'get_beats_confidence', 'get_beats_start',
+       'get_danceability', 'get_duration', 'get_end_of_fade_in',
+       'get_energy', 'get_key', 'get_key_confidence', 'get_loudness',
+       'get_mode', 'get_mode_confidence', 'get_release',
+       'get_release_7digitalid', 'get_sections_confidence',
+       'get_sections_start', 'get_segments_confidence',
+       'get_segments_loudness_max', 'get_segments_loudness_max_time',
+       'get_segments_loudness_start', 'get_segments_pitches',
+       'get_segments_start', 'get_segments_timbre', 'get_similar_artists',
+       'get_song_hotttnesss', 'get_song_id', 'get_start_of_fade_out',
+       'get_tatums_confidence', 'get_tatums_start', 'get_tempo',
+       'get_time_signature', 'get_time_signature_confidence', 'get_title',
+       'get_track_7digitalid', 'get_track_id', 'get_year']
+    """
     cropped_getters = ['title', 'artist_name', 'duration', 'key', 'key_confidence', 'mode',
-                       'mode_confidence', 'tempo', 'time_signature', 'time_signature_confidence', 'year']
+                       'mode_confidence', 'tempo', 'loudness', 'time_signature', 
+                       'time_signature_confidence', 'year', 'sections_start', 'segments_pitches', 
+                       'segments_timbre', 'bars_start', 'bars_confidence', 'tatums_confidence', 
+                       'tatums_start']
 
     # Create a dataframe with the features
     cropped_values = []
@@ -70,23 +95,20 @@ if __name__ == '__main__':
         i = result._index
         if i >= lastIndex + 500:
             lastIndex = i
-            print("num index: " + str(i))
+            print("Songs processed: " + str(i) + "\t|\t " + str(int(i*100/len(pathList))
+                                                                ) + "%\t|\t Time: {:.2f}".format(time.time() - starttime))
 
     for features in result:
         # add each song to the database with key as the title + artist delimited by "\0"
         database[features[0]+"\0"+features[1]] = features
     pool.close()
-    print('That took {} seconds'.format(time.time() - starttime))
-
-
-    starttime = time.time()
+    print('That took {:.2f} seconds'.format(time.time() - starttime))
+    
     # save the database to a pickle file
+    starttime = time.time()
     with open('database.pickle', 'wb') as handle:
         pickle.dump(database, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    print('That took {} seconds'.format(time.time() - starttime))
-
-
-
+    print('That took {:.2f} seconds'.format(time.time() - starttime))
 
     # starttime = time.time()
 
