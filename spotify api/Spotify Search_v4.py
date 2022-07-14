ec331f96ba33776078254c17e2e1c7755f9903b3
Efe T <efe@efetascioglu.com>
2022-07-14 19:05:29 -0400

    Finished The Get_Features Function

--- spotify api/Spotify Search_v4.py
+++ spotify api/Spotify Search_v4.py
@@ -18,7 +18,7 @@ def authentiated_spotipy():
 
 def search(track_name, sp, debug=False):
     '''Returns an array with up to 50 tracks (arrays)
-    Each track-array contains: 
+    Each track-array contains:
     0: song name, 1: artists names, 2: duration, 3: album art, 4: release date, 5: popularity, 6: explicit (T/F), 7: spotify url, 8: id (uri)'''
     result = sp.search(track_name, limit=50, type='track')
     if debug:
@@ -59,27 +59,49 @@ def search(track_name, sp, debug=False):
 
 def get_features(track_id, sp, debug=False):
     '''Returns an array with the features of a track
-    The features returned are: 
+    The features returned are:
     0: Key, 1: Mode, 2: Tempo, 3: Loudness, 4: Time-Signature, 5: Section Starts, 6: Segment Pitches, 7: Segment Timbre, 8: Bars Start, 9: Beats Start 10: Tatums Start'''
     # TODO: What do we actually do with sections start?
     get_audio_features = sp.audio_features(track_id)
     get_audio_analysis = sp.audio_analysis(track_id)
+    # if debug:
+    # print("DEBUG: AUDIO FEATURES:\t", get_audio_features)
+    # print("DEBUG: AUDIO ANALYSIS:\t", get_audio_analysis)
 
     if debug:
-        print("DEBUG: AUDIO FEATURES:\t", get_audio_features)
-        print("DEBUG: AUDIO ANALYSIS:\t", get_audio_analysis)
-
+        print("DEBUG: AUDIO ANALYSIS SECTIONS:\t",
+              get_audio_analysis['sections'][0])
+        print("DEBUG: AUDIO ANALYSIS SEGMENTS:\t",
+              get_audio_analysis['segments'][0])
+        print("DEBUG: AUDIO ANALYSIS BARS:\t",
+              get_audio_analysis['bars'][0])
+        print("DEBUG: AUDIO ANALYSIS BEATS:\t",
+              get_audio_analysis['beats'][0])
+        print("DEBUG: AUDIO ANALYSIS TATUMS:\t",
+              get_audio_analysis['tatums'][0])
+        # print("DEBUG SECTIONS:", {key: [i[key] for i in get_audio_analysis['segments']] for key in get_audio_analysis['segments'][0]})
+        print("DEBUG SEGMENT TIMBRE'S:", [i['timbre']
+              for i in get_audio_analysis['segments']])
     key = get_audio_features[0]['key']
     mode = get_audio_features[0]['mode']
     tempo = get_audio_features[0]['tempo']
     loudness = get_audio_features[0]['loudness']
     time_signature = get_audio_features[0]['time_signature']
-    section_starts = get_audio_analysis['sections'][0]['start']
+    # TODO: Add Year
+    # TODO: Switch Oder
+    '''section_starts = get_audio_analysis['sections'][0]['start']
     segment_pitches = get_audio_analysis['segments'][0]['pitches']
     segment_timbre = get_audio_analysis['segments'][0]['timbre']
     bars_start = get_audio_analysis['bars'][0]['start']
     beats_start = get_audio_analysis['beats'][0]['start']
-    tatums_start = get_audio_analysis['tatums'][0]['start']
+    tatums_start = get_audio_analysis['tatums'][0]['start']'''
+
+    section_starts = [i['start']for i in get_audio_analysis['sections']]
+    segment_pitches = [i['pitches']for i in get_audio_analysis['segments']]
+    segment_timbre = [i['timbre']for i in get_audio_analysis['segments']]
+    bars_start = [i['start']for i in get_audio_analysis['bars']]
+    beats_start = [i['start']for i in get_audio_analysis['beats']]
+    tatums_start = [i['start']for i in get_audio_analysis['tatums']]
 
     result = [key, mode, tempo, loudness, time_signature, section_starts,
               segment_pitches, segment_timbre, bars_start, beats_start, tatums_start]
@@ -88,9 +110,10 @@ def get_features(track_id, sp, debug=False):
 
 if __name__ == "__main__":
     sp = authentiated_spotipy()
-    tracks = search("Smooth Criminal", sp)
+    tracks = search("Eine Kleine Nachtmusik", sp)
     print(tracks[0])
-    print(get_features(tracks[0][8], sp))
+    features = get_features(tracks[0][8], sp)
+    print(len(features[7]), len(features[7][0]))
 
 # TODO: Ceck out the following API fucntons on https://spotipy.readthedocs.io/en/master/#api-reference:
 '''
@@ -100,19 +123,22 @@ current_user_saved_albums(limit=20, offset=0, market=None)
 current_user_saved_tracks(limit=20, offset=0, market=None)
 
 playlist_add_items(playlist_id, items, position=None)
-playlist_change_details(playlist_id, name=None, public=None, collaborative=None, description=None)
+playlist_change_details(playlist_id, name=None, public=None,
+                        collaborative=None, description=None)
 playlist_cover_image(playlist_id)
-playlist_items(playlist_id, fields=None, limit=100, offset=0, market=None, additional_types=('track', 'episode'))
+playlist_items(playlist_id, fields=None, limit=100, offset=0,
+               market=None, additional_types=('track', 'episode'))
 playlist_upload_cover_image(playlist_id, image_b64)
 
-recommendations(seed_artists=None, seed_genres=None, seed_tracks=None, limit=20, country=None, **kwargs)
+recommendations(seed_artists=None, seed_genres=None,
+                seed_tracks=None, limit=20, country=None, **kwargs)
 '''
 
-#start_time = time.time()
-#name = ["Smooth Criminal"]
+# start_time = time.time()
+# name = ["Smooth Criminal"]
 # print(time.time()-start_time)
 # for i in range(len(result)):
-#print("Song", i + 1)
+# print("Song", i + 1)
 # print(result[i])
 
 '''
